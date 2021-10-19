819ab7c4936e316821a6576e195640e0ed3c26a3
Mikolaj Sikorski <mikolaj.sikorski@filestack.com>
2021-10-19 23:32:55 +0200

    setup tests and demo page

---
+++ src/masonry.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import {FC} from "react";
+
+export interface MasonryProps {
+
+}
+
+export const Masonry: FC<MasonryProps> = ({ }) => {
+
+    return (<div>
+
+    </div>)
+
+}
---
+++ tests/masonry.test.ts
@@ -0,0 +1,7 @@
+describe('here should be tests for lib', () => {
+
+    test('Vienna <3 veal', () => {
+        expect('ahh').toBe(true);
+    });
+
+});
