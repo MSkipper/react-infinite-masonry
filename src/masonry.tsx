403ef7252e3312292cd349d5479e06728864858c
Mikolaj Sikorski <mikolaj.sikorski@filestack.com>
2021-11-07 21:50:51 +0100

    masonry layout

--- src/masonry.tsx
+++ src/masonry.tsx
@@ -1,4 +1,4 @@
-import React, {FC, ReactElement} from 'react'
+import React, {FC, ReactElement, useEffect} from 'react'
 
 interface Styles {
 
@@ -16,10 +16,37 @@ export interface MasonryProps {
 
 }
 
+const calculateMasonry = (children, containerSize, columnWidth) => {
+    const length = children.length;
+
+    const columns = Math.round(containerSize/columnWidth);
+
+    const layout = new Array(columns).map((col, index) => {
+        return {
+            left: index * columnWidth,
+            currentTop: 0
+        }
+    });
+    const masonryWrappers = children.map((child, index) => {
+
+        const column = layout.reduce((prev, current) => {
+            return prev.currentTop < current.currentTop ? prev : current;
+        }, {currentTop: 0});
+        const top = column.currentTop;
+
+
+        return (<div className='masonry-item' style={{top: top, left: column.left}}>{child}</div>)
+
+    })
+}
+
 const Masonry: FC<Partial<MasonryProps>> = ({children }) => {
 
     const displayItems = [];
 
+    useEffect(() => {
+        calculateMasonry();
+    }, [children])
 
     return (
     <div data-testid='' >
